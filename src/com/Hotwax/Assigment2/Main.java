08f64ac05222cc7f58f2e0ac69feb9599bffe4a3
Garima <shanuvyas102@gmail.com>
2021-07-04 20:45:42 +0530

    Initial main.java commit

--- src/com/Hotwax/Assigment2/Main.java
+++ src/com/Hotwax/Assigment2/Main.java
@@ -1,9 +1,10 @@
-package com.Hotwax;
+package com.Hotwax.Assigment2;
+
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-
+        System.out.println(" Main Class ");
     }
 }
